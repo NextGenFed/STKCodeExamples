84972fc17782025acc98cf3ed07c241df3f84748
Jeremy Loomis <jloomis@nextgenfed.com>
2022-12-30 19:14:11 -0500

    tweaks to stk-engine-java

--- StkEngineContainerization/windows/stk-engine-java/build.bat
+++ StkEngineContainerization/windows/stk-engine-java/build.bat
@@ -1,1 +1,1 @@
-docker build -t ansys/stk/stk-engine-java:12.5.0-windowsserver-ltsc2022 --build-arg agreeToLicense=yes  .
+docker build -t repo.nextgenfed.com/ngdocker/ansys/stk/stk-engine-java:12.5.0-windowsserver-ltsc2022 .
