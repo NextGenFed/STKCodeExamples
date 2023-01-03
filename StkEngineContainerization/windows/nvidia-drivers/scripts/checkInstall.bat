94de4e679467760287a06d5e138aa25284ee261e
Jeremy Loomis <jloomis@nextgenfed.com>
2023-01-03 09:35:16 -0500

    driver files installed OK

---
+++ StkEngineContainerization/windows/nvidia-drivers/scripts/checkInstall.bat
@@ -0,0 +1,3 @@
+tasklist | find "setup"
+dir C:\windows\inf\set* | find "set"
+dir C:\NvidiaLogging | find "LOG."
--- StkEngineContainerization/windows/nvidia-drivers/scripts/install.ps1
+++ StkEngineContainerization/windows/nvidia-drivers/scripts/install.ps1
@@ -5,17 +5,19 @@ Write-Host $zipFile
 Write-Host "Extracting NVidia setup files"
 Expand-Archive -Path $zipFile[0] -DestinationPath c:/drivers
 Remove-Item $zipFile[0];
-#Write-Host "Extracting NVidia driver files"
-#Expand-Archive -Path $zipFile[1] -DestinationPath c:/drivers
-#Remove-Item $zipFile[1];
 
-sc config PlugPlay start= demand
-sc config DeviceInstall start= demand
-reg import EnableFullLogging.reg
+Write-Host "Enabling DeviceInstall service"
+#sc config PlugPlay start=demand
+#sc config DeviceInstall start=demand
+Set-Service -Name PlugPlay -StartupType Manual
+Set-Service -Name DeviceInstall -StartupType Manual
 
 Write-Host "Running silent install"
+del C:\windows\inf\setupapi*.log
+reg import EnableFullLogging.reg
 $setup = Get-ChildItem -Path c:/drivers -Recurse 'setup.exe' | Select-Object -ExpandProperty FullName
 Write-Host $setup
-Start-Process $setup -Wait -ArgumentList @('Display.Driver -s -clean -noreboot -noeula -log c:\logs')
+#Start-Process $setup -Wait -ArgumentList @('Display.Driver -s -clean -noreboot -noeula -log c:\logs')
+#reg import DisableFullLogging.reg
 
 #Remove-Item -Path C:\drivers\ -Force -Recurse
