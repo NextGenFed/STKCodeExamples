c5967b33350d65da0b5da39792d790475cf95bb3
Jeremy Loomis <jloomis@nextgenfed.com>
2023-01-07 18:25:00 -0500

    try EOIR with OGC

--- StkEngineContainerization/windows/stk-engine-eoir/build.bat
+++ StkEngineContainerization/windows/stk-engine-eoir/build.bat
@@ -1,1 +1,3 @@
-docker build -t ansys/stk/stk-engine-eoir:12.5.0-windowsserver-ltsc2022 .
+REM docker build -t ansys/stk/stk-engine-eoir:12.5.0-windowsserver-ltsc2022 .
+
+docker build --build-arg baseImage=ansys/stk/stk-engine-baseline:12.5.0-server2022-ogc -t ansys/stk/stk-engine-eoir:12.5.0-server2022-ogc .
--- StkEngineContainerization/windows/stk-engine-eoir/run.bat
+++ StkEngineContainerization/windows/stk-engine-eoir/run.bat
@@ -1,1 +1,2 @@
-docker run -it -v c:/Temp:C:/TempHost --name stk-eoir --env-file ..\configuration\licensing.env --rm ansys/stk/stk-engine-eoir:12.5.0-windowsserver-ltsc2022 cmd.exe
+REM docker run -it -v c:/Temp:C:/TempHost --name stk-eoir --env-file ..\configuration\licensing.env --rm ansys/stk/stk-engine-eoir:12.5.0-windowsserver-ltsc2022 cmd.exe
+docker run -it -v c:/Temp:C:/TempHost --name stk-eoir --env-file ..\configuration\licensing.env --rm ansys/stk/stk-engine-eoir:12.5.0-server2022-ogc cmd.exe
