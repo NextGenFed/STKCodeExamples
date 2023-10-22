769b566d4d4d1593f7dd54b9ca6fc7b2d24c630c
Jeremy Loomis <jloomis@nextgenfed.com>
2023-10-22 16:19:36 -0400

    experiment with windows image

--- StkEngineContainerization/windows/stk-engine-baseline/build.bat
+++ StkEngineContainerization/windows/stk-engine-baseline/build.bat
@@ -1,3 +1,4 @@
 REM docker build -t ansys/stk/stk-engine-baseline:12.5.0-windowsservercore-ltsc2022 --build-arg agreeToLicense=yes --build-arg baseImage=mcr.microsoft.com/windows/servercore:ltsc2022 .
 REM docker build -t ansys/stk/stk-engine-baseline:12.5.0-windowsserver-ltsc2022 --build-arg agreeToLicense=yes --build-arg baseImage=mcr.microsoft.com/windows/server:ltsc2022 .
-docker build -t ansys/stk/stk-engine-baseline:12.5.0-server2022-ogc --build-arg agreeToLicense=yes --build-arg baseImage=ansys/stk/opengl-compat:windowsserver-ltsc2022 .
+REM docker build -t ansys/stk/stk-engine-baseline:12.5.0-server2022-ogc --build-arg agreeToLicense=yes --build-arg baseImage=ansys/stk/opengl-compat:windowsserver-ltsc2022 .
+docker build -t ansys/stk/stk-engine-baseline:12.6.0-server2022-ogc --build-arg agreeToLicense=yes --build-arg baseImage=ansys/stk/opengl-compat:windowsserver-ltsc2022 .
--- StkEngineContainerization/windows/stk-engine-baseline/run.bat
+++ StkEngineContainerization/windows/stk-engine-baseline/run.bat
@@ -1,8 +1,10 @@
 REM docker run -it --name stk-baseline --env-file ..\configuration\licensing.env --rm ansys/stk/stk-engine-baseline:12.5.0-windowsservercore-ltsc2022 cmd.exe
 
-REM docker run -it -v c:/Temp:C:/TempHost --name stk-baseline --env-file ..\configuration\licensing.env --rm ansys/stk/stk-engine-baseline:12.5.0-windowsserver-ltsc2022 cmd.exe
+docker run -it -v c:/Temp:C:/TempHost --name stk-baseline --env-file ..\configuration\licensing.env --rm ansys/stk/stk-engine-baseline:12.5.0-windowsserver-ltsc2022 cmd.exe
 
-docker run -it -v c:/Temp:C:/TempHost --name stk-baseline --env-file ..\configuration\licensing.env --rm ansys/stk/stk-engine-baseline:12.5.0-server2022-ogc cmd.exe
+REM docker run -it -v c:/Temp:C:/TempHost --name stk-baseline --env-file ..\configuration\licensing.env --rm ansys/stk/stk-engine-baseline:12.5.0-server2022-ogc cmd.exe
+
+REM docker run -it -v c:/Temp:C:/TempHost --name stk-baseline --env-file ..\configuration\licensing.env --rm ansys/stk/stk-engine-baseline:12.6.0-server2022-ogc cmd.exe
 
 REM ##### with GPUs
 
