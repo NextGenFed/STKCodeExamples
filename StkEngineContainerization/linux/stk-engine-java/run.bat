9a766fb9a7429278dfdb20718af28c6409a9c877
Jeremy Loomis <jloomis@nextgenfed.com>
2023-11-05 11:26:51 -0500

    Java in ubi9

--- StkEngineContainerization/linux/stk-engine-java/run.bat
+++ StkEngineContainerization/linux/stk-engine-java/run.bat
@@ -1,3 +1,8 @@
 REM docker run -it --env-file ../configuration/licensing.env --name stk-javaNG --rm ansys/stk/stk-engine-java-ng:12.4.0-rocky8 sh
 REM docker run -it --env-file ../configuration/licensing.env --name stk-java --rm ansys/stk/stk-engine-java:12.4.0-rocky8 sh
-docker run -it --env-file ../configuration/licensing.env --name stk-java --rm ansys/stk/stk-engine-java:12.4.0-rocky9 sh
+REM docker run -it --env-file ../configuration/licensing.env --name stk-java --rm ansys/stk/stk-engine-java:12.4.0-rocky9 sh
+docker run -it --env-file ../configuration/licensing.env --name stk-java --rm ansys/stk/stk-engine-java:12.6.0-ubi9 sh
+
+REM To test:
+REM cd /home/stk/CodeSamples/CustomApplications/Java/AWT_STK_X_NoGraphics
+REM ant run
