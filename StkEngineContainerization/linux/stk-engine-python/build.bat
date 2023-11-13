8e60d16e4a8cc5acae5dcfc35f0292401e06d5c5
Jeremy Loomis <jloomis@nextgenfed.com>
2023-11-12 20:29:06 -0500

    build stk-engine-python

---
+++ StkEngineContainerization/linux/stk-engine-python/build.bat
@@ -0,0 +1,1 @@
+docker build --build-arg baseImage=ansys/stk/stk-engine-baseline:12.6.0-ubi9 -t ansys/stk/stk-engine-python:12.6.0-ubi9 -f Dockerfile.ubi .
---
+++ StkEngineContainerization/linux/stk-engine-python/run.bat
@@ -0,0 +1,1 @@
+docker run -it --env-file ../configuration/licensing.env --name stk-python --rm ansys/stk/stk-engine-python:12.6.0-ubi9 /bin/sh
