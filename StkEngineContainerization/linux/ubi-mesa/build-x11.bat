90f9f4d2bbc8dd9e5474b06f7b134ee79e33f62a
Jeremy Loomis <jloomis@nextgenfed.com>
2024-02-24 16:28:48 -0500

    add ub-mesa folder with x11 image

---
+++ StkEngineContainerization/linux/ubi-mesa/build-x11.bat
@@ -0,0 +1,1 @@
+docker build -t ubi8:8.8x  -f Dockerfile.x11 .
--- StkEngineContainerization/linux/ubi-mesa/run-x11.sh
+++ StkEngineContainerization/linux/ubi-mesa/run-x11.sh
@@ -1,6 +1,6 @@
-# note: save with Linux EOL chars
+# note: save with Linux EOL char (LF)
 docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
-    --name ubuntu-x-test --rm ubuntu:22.04x  /bin/bash
+    --name ubi-x-test --rm ubi8:8.8x  /bin/bash
 
 # commands to test with
 # cat /etc/os-release
