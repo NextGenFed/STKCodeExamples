0dd58a20c47650d1405b9caf9e522dbdba243aea
Jeremy Loomis <jloomis@nextgenfed.com>
2024-03-03 22:05:29 -0500

    build java:12.8.0-ubi9.2m

--- StkEngineContainerization/linux/stk-engine-java/build.bat
+++ StkEngineContainerization/linux/stk-engine-java/build.bat
@@ -1,4 +1,4 @@
 REM docker build  --build-arg baseImage=ansys/stk/stk-engine-baseline:12.4.0-rocky8 -t ansys/stk/stk-engine-java-ng:12.4.0-rocky8 .
 REM docker build  --build-arg baseImage=ansys/stk/stk-engine-graphics:12.6.0-rocky8 -t ansys/stk/stk-engine-java:12.6.0-rocky8 .
 REM docker build  --build-arg baseImage=ansys/stk/stk-engine-graphics:12.6.0-rocky9 -t ansys/stk/stk-engine-java:12.6.0-rocky9 .
-docker build  --build-arg baseImage=ansys/stk/stk-engine-baseline:12.6.0-ubi9 -t ansys/stk/stk-engine-java:12.6.0-ubi9 .
+docker build  --build-arg baseImage=ansys/stk/stk-engine-baseline:12.8.0-ubi9.2m -t ansys/stk/stk-engine-java:12.8.0-ubi9.2m .
--- StkEngineContainerization/linux/stk-engine-java/run-wsl.sh
+++ StkEngineContainerization/linux/stk-engine-java/run-wsl.sh
@@ -1,4 +1,13 @@
 docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
     --env-file ../configuration/licensing.env \
     -v /mnt/wslg:/mnt/wslg -v /usr/lib/wsl:/usr/lib/wsl --device=/dev/dxg --gpus all \
-    --name stk-java --rm ansys/stk/stk-engine-java:12.6.0-rocky8 sh
+    --name stk-java --rm ansys/stk/stk-engine-java:12.8.0-ubi9.2m sh
+
+
+# testing
+# cd /home/stk/CodeSamples/CustomApplications/Java/AWT_STK_X_NoGraphics
+# /usr/local/ant/bin/ant run
+# cd ../AWT_STK_X_Globe_Basic
+# /usr/local/ant/bin/ant run
+# cd ../AWT_STK_Graphics_Globe_HowTo
+
